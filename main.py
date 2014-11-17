5db07a68644b60e6e8a0b02521effeee6519273d
RostunovSergey <sergey@rostunov.com>
2014-11-17 06:10:20 +0300

    % Small fixes

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from flask import Flask
 from flask import request
+
+from scipy.sparse import *
+from scipy import *
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.svm import LinearSVC
+from sklearn.externals import joblib
+import logging
+import csv
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.kernel_approximation import Nystroem
+import datetime
+
 app = Flask(__name__)
+
+encoder = OneHotEncoder()
+encoder = joblib.load("models/model_00/encoder.pkl")
 
 @app.route("/")
 def hello():
-	return "Hello World!"
-	
+    return "Hello World!"
+
 @app.route('/send_data/<req>', methods=['GET', 'POST'])
 def send_data(req):
-    # if request.method == 'POST':
-    data = request.stream.read().decode("utf-8")
-    return '%s' % data
+    if req == 'learn' and request.method == 'POST':
+        data = request.stream.read().decode("utf-8")
+        return '%s' % data
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
 
